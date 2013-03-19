338d91b0bec8be2a1f6de2930e580022c13251f7
Vitaly Glibin <glibin.v@gmail.com>
2013-03-19 17:30:48 +0400

    Waiters cleanup on message recieve or timeout

--- src/dps_session.erl
+++ src/dps_session.erl
@@ -46,8 +46,6 @@ find_or_create(SessionId, Channels) ->
     Session.
 
 
-
-
 add_channels(Session, Channels) when is_pid(Session) ->
     gen_server:call(Session, {add_channels, Channels}).
 
@@ -57,6 +55,7 @@ fetch(Session, OldSeq) when is_pid(Session) ->
     catch
         exit:{timeout,_} -> {ok, OldSeq, []}
     end.
+
 
 start_link(Session) ->
     gen_server:start_link(?MODULE, Session, []).
@@ -74,27 +73,28 @@ handle_call(fetch, From, State = #state{messages = [], timer = OldTimer, waiters
     Timer = erlang:send_after(?TIMEOUT, self(), timeout),
     {noreply, State#state{waiters = [From|Waiters], timer = Timer}};
 
-
 handle_call({fetch, OldSeq}, _From, State = #state{messages = Messages, timer = OldTimer, seq = Seq}) 
     when OldSeq < Seq ->
-    erlang:cancel_timer(OldTimer),
-    Timer = erlang:send_after(?TIMEOUT, self(), timeout),
-    NewMessages = leave_new(Seq - OldSeq, Messages),
-    {reply, {ok, Seq, lists:reverse(NewMessages)}, State#state{messages = NewMessages, timer = Timer}};
+        erlang:cancel_timer(OldTimer),
+        Timer = erlang:send_after(?TIMEOUT, self(), timeout),
+        NewMessages = leave_new(Seq - OldSeq, Messages),
+        {reply, {ok, Seq, lists:reverse(NewMessages)}, State#state{messages = NewMessages, timer = Timer}};
 
 handle_call({fetch, NewSeq}, From, State = #state{timer = OldTimer, seq = Seq, waiters = Waiters}) 
     when NewSeq >= Seq ->
-    erlang:cancel_timer(OldTimer),
-    Timer = erlang:send_after(?TIMEOUT, self(), timeout),
-    {noreply, State#state{timer = Timer, waiters = [From|Waiters]}};
+        erlang:cancel_timer(OldTimer),
+        Timer = erlang:send_after(?TIMEOUT, self(), timeout),
+        {noreply, State#state{timer = Timer, waiters = [From|Waiters]}};
 
 handle_call({add_channels, Channels}, _From, State = #state{
                                                     channels = OldChannels}) ->
     [dps:subscribe(Channel) || Channel <- Channels -- OldChannels],
     NewChannels = sets:to_list(sets:from_list(Channels ++ OldChannels)),
     {reply, NewChannels, State#state{channels = NewChannels}};
+
 handle_call(Msg, _From, State) ->
     {reply, {error, {unknown_call, Msg}}, State}.
+
 
 handle_cast(_Msg, State) ->
     {noreply, State}.
@@ -106,13 +106,15 @@ handle_info({dps_msg, _Tag, _LastTS, NewMessages}, State = #state{waiters = Wait
         Len when Len >= 2*Limit -> element(1,lists:split(Limit,NewMessages ++ Messages));
         _ -> NewMessages ++ Messages
     end,
-    {noreply, State#state{seq = Seq + length(NewMessages), messages = Messages1}};
+    {noreply, State#state{seq = Seq + length(NewMessages), messages = Messages1, waiters = []}};
 handle_info(timeout, #state{waiters = []} = State) ->
+    %%erlang:display("No waiters"),
     {stop, normal, State};
-handle_info(timeout, #state{timer = OldTimer} = State) ->
+handle_info(timeout, #state{timer = OldTimer, waiters = Waiters} = State) ->
     erlang:cancel_timer(OldTimer),
     Timer = erlang:send_after(?TIMEOUT, self(), timeout),
-    {noreply, State#state{timer = Timer}};
+    %%erlang:display(io:format("With timer ~p:~p~n", [Session, Waiters])),
+    {noreply, State#state{timer = Timer, waiters = lists:filter(fun({P, _}) -> is_process_alive(P) end, Waiters)}};
 handle_info(_Info, State) ->
     {stop, {unknown_message, _Info}, State}.
 
